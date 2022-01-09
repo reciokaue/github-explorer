08fe2122d0048eaaf47ffd1198c2b0f09cc2bca6
Kaue Recio <kaue.recio2@gmail.com>
2022-01-09 18:11:01 -0300

    first commit

---
+++ src/components/Card/CardAnimation/index.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useWindowDimensions, ViewProps } from 'react-native';
+import {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
+
+import { AnimationContainer } from './styles';
+
+interface CardAnimationProps extends ViewProps {
+  children: React.ReactNode;
+}
+
+export function CardAnimation({ children, ...rest }: CardAnimationProps) {
+  const { width: displayWidth } = useWindowDimensions();
+  const cardOpacity = useSharedValue(0);
+  const cardOffset = useSharedValue(0.25 * 750);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      // opacity: interpolate(cardOpacity.value, [0, 50], [0, 1]),
+      // transform: [{translateX: interpolate(cardOffset.value,
+      //   [0, 50], [0.25 * 750, 0], Extrapolate.CLAMP  
+      // )}]
+      opacity: cardOpacity.value,
+      transform: [{translateX: cardOffset.value}]
+    }
+  })
+
+  useEffect(() => {
+    cardOpacity.value = withTiming(1, {duration: 1000})
+    cardOffset.value = withTiming(0, {duration: 1000})
+  }, []);
+
+  return (
+    <AnimationContainer {...rest} style={animatedStyle}>
+      {children}
+    </AnimationContainer>
+  )
+}
